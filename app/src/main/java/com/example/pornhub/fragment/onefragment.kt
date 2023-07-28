43f5cea7d843f9adbef04c9edea9b950020274cc
Jean <jepiosbu@gmail.com>
2023-07-28 13:09:17 -0500

    Proyecto subterminado fragments actualizados

--- app/src/main/java/com/example/pornhub/fragment/onefragment.kt
+++ app/src/main/java/com/example/pornhub/fragment/onefragment.kt
@@ -1,46 +1,33 @@
 package com.example.pornhub.fragment
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
+import androidx.cardview.widget.CardView
 import com.example.pornhub.R
 import com.example.pornhub.SplahsActivity
+import com.example.pornhub.StateActivity
+import com.example.pornhub.databinding.FragmentOnefragmentBinding
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 
 class onefragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentOnefragmentBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_onefragment, container, false)
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            onefragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        _binding= FragmentOnefragmentBinding.inflate(inflater,container,false)
+        return binding.root
     }
 }
